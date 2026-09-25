6dee89533b8ce21e2e9503c32d6043e312a7dcd1
matritze87 <matritzzze@gmail.com>
2026-09-26 01:20:31 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE='aiwar-check-v3';
+const ASSETS=['./','./index.html','./manifest.json'];
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request)
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return r;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
